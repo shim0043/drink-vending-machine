931f8483454ae759e58c20206fb48182190c6bbc
Shim Kiseoung <shim0043@algonquinlive.com>
2026-04-17 12:39:51 -0400

    Adds js content

---
+++ script.js
@@ -0,0 +1,11 @@
+const $balanceDisplay = document.getElementById('balance-display');
+const $moneyInput = document.getElementById('money-input');
+const $moneyWarning = document.getElementById('money-warning');
+
+const $vendingSlot = document.getElementById('vending-slot');
+const $statusText = document.getElementById('status-text');
+const $inventoryList = document.getElementById('inventory-list');
+
+const $btnAddToBag = document.getElementById('add-to-bag');
+const $btnThrowAway = document.getElementById('throw-away');
+const $btnReset = document.getElementById('system-reset');
